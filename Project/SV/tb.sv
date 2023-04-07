268d54149e3c867cd5f19b25f79a3fbc1fc4cbfa
CInSang <cinsang@okstate.edu>
2023-04-07 15:15:59 -0500

    Update tb.sv

--- Project/SV/tb.sv
+++ Project/SV/tb.sv
@@ -1,5 +1,48 @@
-module
+module datapath ();
 
-assign grid = 64'h0412_6424_0034_3C28;
+   logic  grid;
+   logic grid_evolve;
 
-end
+   
+   integer handle3;
+   integer desc3;
+   
+   // Instantiate DUT
+   dfatapath dut (grid, reset, left, right, LA, LB, LC, RA, RB, RC);   
+   
+   // Setup the clock to toggle every 1 time units 
+   initial 
+     begin	
+	clk = 1'b1;
+	forever #5 clk = ~clk;
+     end
+
+   initial
+     begin
+	// Gives output file name
+	handle3 = $fopen("fsm.out");
+	// Tells when to finish simulation
+	#700 $finish;		
+     end
+
+   always 
+     begin
+	desc3 = handle3;
+	#5 $fdisplay(desc3, "%b %b || %b %b %b || %b || %b %b %b", 
+		     reset, left, LA, LB, LC, right, RA, RB, RC);
+     end   
+   
+   initial 
+     begin      
+	#0   reset = 1'b1;
+	#10  reset = 1'b0;	
+	#0   right = 1'b1;
+     #0   left = 1'b1;
+	#30  right = 1'b0;
+     #0   left = 1'b1;
+	#30  left = 1'b0;
+     #0   right = 1'b1;
+     #0   left = 1'b0;
+     end
+
+endmodule 
