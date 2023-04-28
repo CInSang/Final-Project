ebde06a97b60e6e59ca6ce5f42500503fe072f63
CInSang <cinsang@okstate.edu>
2023-04-28 14:55:31 -0500

    Update control.sv

--- Project/SV/control.sv
+++ Project/SV/control.sv
@@ -1,33 +1,19 @@
 
 
-module control (clk, start, Mux, grid, HDMI);
+module control (clk, Datapath, reset, HDMI);
 
-  input logic  clk;
-  input logic start;
-  input logic Mux;  
-  input logic grid;
+  input logic  Datapath;
+  input logic clk;
+  input logic reset;  
   output logic HDMI;
-  
-  assign grid = 64'h0412_6424_0034_3C28;
-  
-   // state register
-  always_ff @(posedge clk, posedge reset)
-   if (reset) 
-      HDMI <= next_grid;
-   else      
-      HDMI <= grid;
-	//The next state logic
-16		always @*
-17			if(load)
-18				HDMI_next = grid_evolve;
-19			else if ()
-20				HDMI_next = HDMI;
-21			else if ()
-22				HDMI_next = HDMI + 8'h01;
-23			else 
-24				HDMI_next = HDMI - 8'h01;
 
-   
+  always @(posedge clk ) 
+  if(reset) begin
+    HDMI <= 1'b0;
+  end
+   else begin
+    HDMI <= Datapath;
+   end
 endmodule
 
 
